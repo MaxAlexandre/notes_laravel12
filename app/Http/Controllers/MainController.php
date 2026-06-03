0b9fded83ffbc90de300e326cd21ed525267e14b
Max Ferreira <max.alexxandre@gmail.com>
2026-06-03 17:35:50 -0300

    Implementando middlewares nas rotas e criando página inicial da aplicação.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,4 +7,16 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
 
+    public function index()
+    {
+        // load user's notes
+
+        // show home view
+        return view('home');
+    }
+
+    public function newNote()
+    {
+        echo 'Criar nota';
+    }
 }
--- resources/views/main.blade.php
+++
@@ -1,3 +0,0 @@
-
-<h1>Welcome View Blade</h1>
-
